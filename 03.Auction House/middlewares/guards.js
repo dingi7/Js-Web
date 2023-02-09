3954cd4a6773a4ef72d3bf54ce93142c50da4b36
dingi7 <kamenkanev88@gmail.com>
2023-02-09 22:49:56 +0200

    Implemented dynamic errors

--- 03.Auction House/middlewares/guards.js
+++ 03.Auction House/middlewares/guards.js
@@ -1,4 +1,4 @@
-function isAuth(){
+function isAuth() {
     return (req, res, next) => {
         if (req.user != undefined) {
             next();
@@ -21,10 +21,10 @@ function isGuest() {
 function isOwner() {
     return async (req, res, next) => {
         const auction = await req.storage.getAuctionById(req.params.id);
-        if (auction && req.user && (auction.owner._id == req.user._id)) {
-            next()
+        if (auction && req.user && auction.owner._id == req.user._id) {
+            next();
         } else {
-            res.redirect('/')
+            res.redirect('/');
         }
     };
 }
@@ -32,34 +32,55 @@ function isOwner() {
 function isNotOwner() {
     return async (req, res, next) => {
         const auction = await req.storage.getAuctionById(req.params.id);
-        if (auction && req.user && (auction.owner._id == req.user._id)) {
-            res.redirect('/')
+        if (auction && req.user && auction.owner._id == req.user._id) {
+            res.redirect('/');
         } else {
-            next()
+            next();
         }
     };
 }
 
-function isNotBid(){
-    return async (req,res,next) => {
+function isNotBid() {
+    return async (req, res, next) => {
         const auction = await req.storage.getAuctionById(req.params.id);
-        if (auction.bidder && req.user && (auction.bidder._id == req.user._id) || (auction.price >= req.body.amount)) {
-            res.render('details', {title: "Details", error: "Some error accured", auction})
+        if (
+            (auction.bidder &&
+                req.user &&
+                auction.bidder._id == req.user._id) ||
+            auction.price >= req.body.amount
+        ) {
+            if (auction.bidder._id == req.user._id) {
+                return res.render('details', {
+                    title: 'Details',
+                    error: 'Error. You have already bid on this auction.',
+                    auction,
+                });
+            } else if (auction.price >= req.body.amount) {
+                return res.render('details', {
+                    title: 'Details',
+                    error: 'Error. You are trying to bid less than the current bid.',
+                    auction,
+                });
+            }
         } else {
-            next()
+            next();
         }
-    }
+    };
 }
 
-function anyBids(){
-    return async (req,res,next) => {
+function anyBids() {
+    return async (req, res, next) => {
         const auction = await req.storage.getAuctionById(req.params.id);
         if (!auction.bidder) {
-            res.render('details', {title: "Details", error: "Some error accured", auction})
+            res.render('details', {
+                title: 'Details',
+                error: 'Auction cannot be closed, since there are no bids yet.',
+                auction,
+            });
         } else {
-            next()
+            next();
         }
-    }
+    };
 }
 
 module.exports = {
@@ -68,5 +89,5 @@ module.exports = {
     isOwner,
     isNotOwner,
     isNotBid,
-    anyBids
+    anyBids,
 };
